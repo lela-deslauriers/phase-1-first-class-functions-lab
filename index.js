cf1783a2c240830ebacb127c7140a062163aa1d1
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-02 23:36:13 -0800

    commit tree 3ad346893982961fff2ad3e1396ea07f158736be

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2); //first argument in slice is the index/start, and the second argument indicates how many values to return in the array
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => {
+    return (fare) => fare * multiplier;
+    }
+
+/*
+const createFareMultiplier = function (multiplier) {
+    const fareMultiplier = function (fare) {
+        multiplier * fare;
+    } 
+    return fareMultiplier 
+}
+
+*/
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => returnFirstTwoDrivers(drivers);
